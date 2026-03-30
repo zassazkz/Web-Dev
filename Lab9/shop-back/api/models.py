4f18032be42d4816fdd3b7d8d08dc0408b5b8a09
zassazkz <zassazkz@gmail.com>
2026-03-30 14:55:00 +0500

    Lab 9: Django REST Framework

---
+++ Lab9/shop-back/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
